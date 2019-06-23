ae9245ed21893571200140ea53059f4c85c144b2
Richard Guan <rguan72@gmail.com>
2019-06-23 16:51:06 -0400

    Create merchant table

--- storeback/models/inventories.py
+++ storeback/models/inventories.py
@@ -1,5 +1,6 @@
-from . import db
 import uuid
+from datetime import datetime
+from . import db
 
 
 class Inventory(db.Model):
@@ -7,12 +8,16 @@ class Inventory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
     price = db.Column(db.Integer)
+    merchant_id = db.Column(db.Integer, db.ForeignKey('merchant.id'))
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+    updated = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     def to_json(self):
         res = {}
-        for field in ('id', 'name', 'price'):
+        for field in ('id', 'name', 'price', 'created', 'updated'):
             value = getattr(self, field)
             res[field] = value
+        res['merchant'] = self.merchant.to_json()
 
         return res
 
---
+++ storeback/models/merchants.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from . import db
+from .inventories import Inventory
+
+
+class Merchant(db.Model):
+    __tablename__ = 'merchant'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    inventory = db.relationship('Inventory', backref='merchant', lazy=True)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+    updated = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def to_json(self):
+        res = {}
+        for field in ('id', 'name', 'created', 'updated'):
+            res[field] = getattr(self, field)
+            
+        return res
+
+    def __repr__(self):
+        return f'Merchant {self.name}'
