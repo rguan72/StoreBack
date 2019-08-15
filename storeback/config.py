f28d3e88b6b02d97aa268a646cf0295a8a2e19f1
Richard Guan <rguan72@gmail.com>
2019-08-15 00:37:32 -0400

    Allow compatibility with mysql

--- storeback/config.py
+++ storeback/config.py
@@ -3,7 +3,7 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config():
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or \
         'sqlite:///' + os.path.join(base_dir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASK_APP = os.environ.get('FLASK_APP') or 'storeback/wsgi.py'
