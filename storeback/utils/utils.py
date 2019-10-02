8b2e2809b6fafca4d9dc394b6991af8a3677f864
Richard Guan <rguan72@gmail.com>
2019-10-02 13:54:52 -0400

    Change to handle case when headers are not present

--- storeback/utils/utils.py
+++ storeback/utils/utils.py
@@ -3,12 +3,12 @@ from storeback.models.keys import Key
 from .constants import APIKEY_HEADER, KEYCODE_HEADER
 
 def get_admin_id_from_headers():
-    api_key = request.headers[APIKEY_HEADER]
-    key_code = request.headers[KEYCODE_HEADER]
+    api_key = request.headers.get(APIKEY_HEADER)    
+    key_code = request.headers.get(KEYCODE_HEADER)
     if not api_key or not key_code:
-        abort(404, description="no items found")
+        abort(404, description="No items found.")
     found_key = Key.query.filter_by(key_code=key_code).first_or_404()
     if Key.verify_hash(api_key, found_key.value):
         return found_key.admin_id
     else:
-        abort(404, description="no items found")
+        abort(404, description="No items found.")
